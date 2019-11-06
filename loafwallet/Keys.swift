280a9ba447ac7c11a460116bd89d5b6e4e25d9cc
Kerry Washington <mrkerrywashington@icloud.com>
2019-11-06 11:41:47 -0800

    mp setup

---
+++ loafwallet/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  loafwallet
+//
+//  Created by Kerry Washington on 11/6/19.
+//  Copyright © 2019 Litecoin Foundation. All rights reserved.
+//
+
+import UIKit
+
+struct K {
+    static let mixpanelToken = "7773cfa76d8f0a30772a5e9d1802736d"
+}
