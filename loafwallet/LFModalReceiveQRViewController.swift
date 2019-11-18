f272cb03b6a0c3f0d809b81a1ce3a1ab70a63301
Kerry Washington <mrkerrywashington@icloud.com>
2019-11-17 19:13:45 -0800

    WIP 8 Errors bring in QR Code

---
+++ loafwallet/LFModalReceiveQRViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LFModalReceiveQRViewController.swift
+//  loafwallet
+//
+//  Created by Kerry Washington on 11/17/19.
+//  Copyright © 2019 Litecoin Foundation. All rights reserved.
+//
+
+import UIKit
+
+class LFModalReceiveQRViewController: UIViewController {
+    
+}
