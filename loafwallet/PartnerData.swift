ef2f291d18e32f2002452ba2f201bc30f459c228
kcw-grunt <mrkerrywashington@icloud.com>
2021-05-04 13:51:53 +0100

    Refactored file partner data
    - Added file structure

--- loafwallet/PartnerData.swift
+++ loafwallet/PartnerData.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 import UIKit
+
+
+enum PartnerName {
+    case infura
+    case changeNow
+}
  
 struct Partner {
     
@@ -19,6 +25,32 @@ struct Partner {
         let moonpay = Partner(logo: UIImage(named: "moonpay-logo")!, headerTitle: S.BuyCenter.Cells.moonpayTitle, details: S.BuyCenter.Cells.moonpayFinancialDetails)
         let simplex = Partner(logo: UIImage(named: "simplexLogo")!, headerTitle: S.BuyCenter.Cells.simplexTitle, details: S.BuyCenter.Cells.simplexFinancialDetails)
         return [moonpay, simplex]
+    }
+    
+    static func partnerKeyPath(name: PartnerName) -> String {
+        
+        switch name {
+            case .infura:
+                
+                if let path = Bundle.main.path(forResource: "partner-keys", ofType: "plist"),
+                          let dictionary = NSDictionary(contentsOfFile:path) as? Dictionary<String, AnyObject>,
+                          let key = dictionary["infura-api"] as? String {
+                           return "https://mainnet.infura.io/v3/" + key
+                       } else {
+                           return "ERROR-INFURA_KEY"
+                       }
+                 
+            case .changeNow:
+                
+                if let path = Bundle.main.path(forResource: "partner-keys", ofType: "plist"),
+                   let dictionary = NSDictionary(contentsOfFile:path) as? Dictionary<String, AnyObject>,
+                   let key = dictionary["change-now-api"] as? String {
+                    return key
+                } else {
+                    return "ERROR-CHANGENOW_KEY"
+                }
+        }
+        
     }
      
 //TODO: Uncomment as integration progresses, kcw-grunt
@@ -39,3 +71,43 @@ struct Partner {
 //       "logo":UIImage(named:"bitrefillLogo") ?? " ",
 //       "baseColor":#colorLiteral(red: 0.2235294118, green: 0.5490196078, blue: 0.9333333333, alpha: 1)] as [String : AnyObject]
 }
+
+//
+//
+//private func fetchKeyPath()  -> String? {
+//    
+//    if let path = Bundle.main.path(forResource: "partner-keys", ofType: "plist"),
+//       let dictionary = NSDictionary(contentsOfFile:path) as? Dictionary<String, AnyObject>,
+//       let key = dictionary["infura-api"] as? String {
+//        return "https://mainnet.infura.io/v3/" + key
+//    } else {
+//        return nil
+//    }
+//    
+//    
+//    private func setFirebaseConfiguration() {
+//        
+//        var partnerFilePath: String = ""
+//        
+//        #if Release
+//        guard let releasePath = Bundle.main.path(forResource: "file-resource/release/GoogleService-Info", ofType: "plist") else { return
+//        }
+//        
+//        filePath = releasePath
+//        
+//        #else
+//        guard let debugPath = Bundle.main.path(forResource: "file-resource/release/GoogleService-Info", ofType: "plist") else { return
+//            return
+//        }
+//        
+//        filePath = debugPath
+//        
+//        #endif
+//        
+//        guard let fileopts = FirebaseOptions(contentsOfFile: filePath) else {
+//            assert(false, "Couldn't load config file")
+//        }
+//        
+//        FirebaseApp.configure(options: fileopts)
+//    }
+//}
