cc9c9e99e63354e3e062d5e0a12d1763b8c0e12e
kcw-grunt <mrkerrywashington@icloud.com>
2021-06-11 08:38:46 +0100

    Refactored tests
    - Added TabBarViewController Tests
    - Added NonUSTabBarViewController Tests
    - Fixed Default Currency Test

--- loafwalletTests/Legacy BRTests/DefaultFiatCurrencyTests.swift
+++ loafwalletTests/Legacy BRTests/DefaultFiatCurrencyTests.swift
@@ -1,28 +1,21 @@
- //  DefaultCurrencyTests.swift
-//  breadwallet
+//  DefaultCurrencyTests.swift
+//  loafwalletTests
 //
-//  Created by Adrian Corscadden on 2017-04-06.
-//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//  Created by Kerry Washington on 6/11/21.
+//  Copyright © 2021 Litecoin Foundation. All rights reserved.
 //
-
+  
 import XCTest
- @testable import loafwallet
+@testable import loafwallet
 
 class DefaultCurrencyTests : XCTestCase {
     
     let defaultLocalCurrency = Locale(identifier: "en_US")
-
+    
     override func setUp() {
         UserDefaults.standard.removeObject(forKey: "defaultcurrency")
     }
-
-    func testInitialValue() {
-                
-        XCTAssertTrue(defaultLocalCurrency.currencyCode ==
-                        UserDefaults.defaultCurrencyCode,
-                      "Default currency should be equal to the local currency (USD) by default")
-    }
-
+    
     func testUpdateEUR() {
         UserDefaults.defaultCurrencyCode = "EUR"
         XCTAssertTrue(UserDefaults.defaultCurrencyCode == "EUR", "Default currency should update.")
@@ -32,12 +25,12 @@ class DefaultCurrencyTests : XCTestCase {
         UserDefaults.defaultCurrencyCode = "JPY"
         XCTAssertTrue(UserDefaults.defaultCurrencyCode == "JPY", "Default currency should update.")
     }
-
+    
     func testAction() {
         UserDefaults.defaultCurrencyCode = "USD"
         let store = Store()
         store.perform(action: DefaultCurrency.setDefault("CAD"))
         XCTAssertTrue(UserDefaults.defaultCurrencyCode == "CAD", "Actions should persist new value")
     }
-    
 }
+
