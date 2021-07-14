6b941e7638d93b990a4eeb7eb60a4f0d1523c3ee
Jasper Heist <jcheist12@gmail.com>
2021-07-14 19:00:16 -0400

    deleted out some commented code
    removed some BITID references

--- loafwallet/src/FlowControllers/URLController.swift
+++ loafwallet/src/FlowControllers/URLController.swift
@@ -64,11 +64,6 @@ class URLController : Trackable {
             return true
         case "litecoin":
             return handleBitcoinUri(url)
-//        case "bitid":
-//            if BRBitID.isBitIDURL(url) {
-//                handleBitId(url)
-//            }
-//            return true
         default:
             return false
         }
@@ -102,27 +97,6 @@ class URLController : Trackable {
             return false
         }
     }
-
-//    private func handleBitId(_ url: URL) {
-//        let bitid = BRBitID(url: url, walletManager: walletManager)
-//        let message = String(format: S.BitID.authenticationRequest, bitid.siteName)
-//        let alert = UIAlertController(title: S.BitID.title, message: message, preferredStyle: .alert)
-//        alert.addAction(UIAlertAction(title: S.BitID.deny, style: .cancel, handler: nil))
-//        alert.addAction(UIAlertAction(title: S.BitID.approve, style: .default, handler: { _ in
-//            bitid.runCallback(store: self.store) { data, response, error in
-//                if let resp = response as? HTTPURLResponse, error == nil && resp.statusCode >= 200 && resp.statusCode < 300 {
-//                    let alert = UIAlertController(title: S.BitID.success, message: nil, preferredStyle: .alert)
-//                    alert.addAction(UIAlertAction(title: S.Button.ok, style: .default, handler: nil))
-//                    self.present(alert: alert)
-//                } else {
-//                    let alert = UIAlertController(title: S.BitID.error, message: S.BitID.errorMessage, preferredStyle: .alert)
-//                    alert.addAction(UIAlertAction(title: S.Button.ok, style: .default, handler: nil))
-//                    self.present(alert: alert)
-//                }
-//            }
-//        }))
-//        present(alert: alert)
-//    }
 
     private func present(alert: UIAlertController) {
         store.trigger(name: .showAlert(alert))
