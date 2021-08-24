60365a2f0d76608223eea54fb04c1edcb46e4c22
kcw-grunt <mrkerrywashington@icloud.com>
2021-08-24 10:11:47 +0100

    Resolved per PR notes

--- loafwalletTests/Two Factor Auth Tests/Enter2FACodeViewModelTests.swift
+++ loafwalletTests/Two Factor Auth Tests/Enter2FACodeViewModelTests.swift
@@ -17,22 +17,6 @@ class Enter2FACodeViewModelTests: XCTestCase {
         super.setUp()
         viewModel = Enter2FACodeViewModel()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
     /// Checks the user taps on the closure
     func testDidConfirmToken() throws {
