2cf8d691641cc6e72d39f786c3f8aec4114a8bf7
kcw-grunt <mrkerrywashington@icloud.com>
2021-08-25 19:54:24 +0100

    Added tests
    - Stubbed in the View Extenstion tests
    - Enable2FAViewModel tests

---
+++ loafwalletTests/View Extension Tests/ViewExtensionTests.swift
@@ -0,0 +1,52 @@
+//
+//  ViewExtensionTests.swift
+//  loafwalletTests
+//
+//  Created by Kerry Washington on 8/15/21.
+//  Copyright © 2021 Litecoin Foundation. All rights reserved.
+//
+
+import XCTest
+import SwiftUI
+@testable import loafwallet 
+
+class ViewExtensionTests: XCTestCase {
+    
+     
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testEnableCodeView() throws {
+        //TODO: Write basic test
+    }
+    
+    func testLoginAlertView() throws {
+        //TODO: Write basic test
+    }
+    
+    func testForgotPasswordView() throws {
+        //TODO: Write basic test
+    }
+    
+    func testRegisteredAlertView() throws {
+        //TODO: Write basic test
+    } 
+}
