afa4800345257ce4e16798c330503e396be8302b
kcw-grunt <mrkerrywashington@icloud.com>
2022-06-05 21:30:44 -0700

    Added basic new send UIUX

---
+++ loafwallet/SendUIHostingController.swift
@@ -0,0 +1,22 @@
+//
+//  SendUIHostingController.swift
+//  loafwallet
+//
+//  Created by Kerry Washington on 6/4/22.
+//  Copyright © 2022 Litecoin Foundation. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+
+class SendUIHostingController : UIHostingController<SendSwiftUIView> {
+    
+    @ObservedObject
+    var sendViewModel = SendSwiftUIViewModel()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder, rootView: SendSwiftUIView(isReadyToSend: .constant(false)))
+    }
+}
