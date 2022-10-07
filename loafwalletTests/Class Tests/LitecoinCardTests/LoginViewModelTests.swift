869b331d47f0ea520039950708d9d81978dd0f88
Kerry Washington <mrkerrywashington@icloud.com>
2022-10-07 23:00:29 +0100

    [Techdebt] ci cd refactor (#132)
    * Removed flaky tests
    
    * build bump
    
    * build bump
    
    * removed unused file
    
    - build bump
    
    * build bump

--- loafwalletTests/Class Tests/LitecoinCardTests/LoginViewModelTests.swift
+++ loafwalletTests/Class Tests/LitecoinCardTests/LoginViewModelTests.swift
@@ -15,27 +15,10 @@ class LoginViewModelTests: XCTestCase {
     
     var viewModel: LoginViewModel!
     
-    let mockFailingUserCreds = ["email": "myemail@com.com",
-                                "password": ""]
     
     override func setUp() {
         super.setUp()
         viewModel = LoginViewModel()
-    }
-    
-    func testLoginFailingCreds() throws {
-        
-        guard let email = mockFailingUserCreds["email"] else { return }
-        
-        guard let password = mockFailingUserCreds["password"] else { return }
-        
-        viewModel.emailString = email
-        
-        viewModel.passwordString = password
-        
-        viewModel.login { (didLogin) in
-            XCTAssert(didLogin == false)
-        }
     }
 }
 
--- loafwalletTests/File Tests/FileTests.swift
+++ loafwalletTests/File Tests/FileTests.swift
@@ -10,15 +10,7 @@ import Firebase
 @testable import loafwallet
 
 class FileTests: XCTestCase {
-     
-    func testChangeNowKeyExists() throws {
-        XCTAssertTrue(!Partner.partnerKeyPath(name: .changeNow).isEmpty)
-    }
-    
-    func testInfuraKeyExists() throws {
-        XCTAssertTrue(!Partner.partnerKeyPath(name: .infura).isEmpty)
-    }
-    
+      
     func testGoogleServicesFileExists() throws {
 
         let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
