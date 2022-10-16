07a33780f66cd96a10f12c5a4bc3e550196b51b6
Kerry Washington <mrkerrywashington@icloud.com>
2022-10-16 12:05:20 +0100

    Feature/add bitrefill new (#134)
    * Added llocalView
    
    - added bitrefill
    - basic function is available
    - build bump
    
    * bump
    
    * added basic web widget
    
    * updated logo
    
    * build bump
    
    * changed the Bitrefill link
    
    - build bump
    
    * pre-bump version
    
    - to get a binary to TestFlight

--- loafwallet/PartnerData.swift
+++ loafwallet/PartnerData.swift
@@ -23,16 +23,13 @@ struct Partner {
     /// Fills partner data
     /// - Returns: Array of Partner Data
     static func partnerDataArray() -> [Partner] {
+        
+        let bitrefill = Partner(logo: UIImage(named: "bitrefillLogo")!, headerTitle: S.BuyCenter.Cells.bitrefillTitle, details: S.BuyCenter.Cells.bitrefillFinancialDetails)
         let moonpay = Partner(logo: UIImage(named: "moonpay-logo")!, headerTitle: S.BuyCenter.Cells.moonpayTitle, details: S.BuyCenter.Cells.moonpayFinancialDetails)
         let simplex = Partner(logo: UIImage(named: "simplexLogo")!, headerTitle: S.BuyCenter.Cells.simplexTitle, details: S.BuyCenter.Cells.simplexFinancialDetails)
-        return [moonpay, simplex]
+        return [bitrefill, moonpay, simplex]
     }
     
-    //TODO: Uncomment as integration progresses, kcw-grunt
-    //    let bitrefillDictionary =
-    //      ["title":S.BuyCenter.Cells.bitrefillTitle as AnyObject,
-    //       "details":S.BuyCenter.Cells.bitrefillFinancialDetails,
-    //       "logo":UIImage(named:"bitrefillLogo") ?? " ",
-    //       "baseColor":#colorLiteral(red: 0.2235294118, green: 0.5490196078, blue: 0.9333333333, alpha: 1)] as [String : AnyObject]
+  
 }
 
