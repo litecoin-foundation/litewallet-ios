a4f14376fdc1b6b992476bc55e93f79181ace69b
kcw-grunt <mrkerrywashington@icloud.com>
2022-11-07 15:16:21 +0000

    changed the urtl
    - change the deployment target

--- loafwallet/src/Platform/BRAPIClient.swift
+++ loafwallet/src/Platform/BRAPIClient.swift
@@ -70,7 +70,7 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
     var proto = "https"
     
     // host is the server(s) on which the API is hosted
-    var host = "api.loafwallet.org"
+    var host = "api-prod.lite-wallet.org"
     
     // isFetchingAuth is set to true when a request is currently trying to renew authentication (the token)
     // it is useful because fetching auth is not idempotent and not reentrant, so at most one auth attempt
