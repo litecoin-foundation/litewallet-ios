d6214dfd3d46c757c6f34cbfd770ef89581dbb5a
Ivan Ferenčak <iferencak@profico.hr>
2022-11-29 10:03:36 +0100

    PartnerData auto-format

--- loafwallet/PartnerData.swift
+++ loafwallet/PartnerData.swift
@@ -7,19 +7,17 @@ enum PartnerName {
 }
 
 struct Partner {
-    
-    let logo: UIImage
-    let headerTitle: String
-    let details: String
-    
-    /// Fills partner data
-    /// - Returns: Array of Partner Data
-    static func partnerDataArray() -> [Partner] {
-        
-        let bitrefill = Partner(logo: UIImage(named: "bitrefillLogo")!, headerTitle: S.BuyCenter.Cells.bitrefillTitle, details: S.BuyCenter.Cells.bitrefillFinancialDetails)
-        let moonpay = Partner(logo: UIImage(named: "moonpay-logo")!, headerTitle: S.BuyCenter.Cells.moonpayTitle, details: S.BuyCenter.Cells.moonpayFinancialDetails)
-        let simplex = Partner(logo: UIImage(named: "simplexLogo")!, headerTitle: S.BuyCenter.Cells.simplexTitle, details: S.BuyCenter.Cells.simplexFinancialDetails)
-        
-        return [bitrefill, moonpay, simplex]
-    }
+	let logo: UIImage
+	let headerTitle: String
+	let details: String
+
+	/// Fills partner data
+	/// - Returns: Array of Partner Data
+	static func partnerDataArray() -> [Partner] {
+		let bitrefill = Partner(logo: UIImage(named: "bitrefillLogo")!, headerTitle: S.BuyCenter.Cells.bitrefillTitle, details: S.BuyCenter.Cells.bitrefillFinancialDetails)
+		let moonpay = Partner(logo: UIImage(named: "moonpay-logo")!, headerTitle: S.BuyCenter.Cells.moonpayTitle, details: S.BuyCenter.Cells.moonpayFinancialDetails)
+		let simplex = Partner(logo: UIImage(named: "simplexLogo")!, headerTitle: S.BuyCenter.Cells.simplexTitle, details: S.BuyCenter.Cells.simplexFinancialDetails)
+
+		return [bitrefill, moonpay, simplex]
+	}
 }
