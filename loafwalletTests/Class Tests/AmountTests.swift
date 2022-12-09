94de972f61ba189e24440adab134dc83d93e63c7
kcw-grunt <mrkerrywashington@icloud.com>
2022-12-09 14:09:57 +0000

    small change build bump

--- loafwalletTests/Class Tests/AmountTests.swift
+++ loafwalletTests/Class Tests/AmountTests.swift
@@ -2,19 +2,11 @@
 import XCTest
 
 class AmountTests: XCTestCase {
-	override func setUp() {
-		// Put setup code here. This method is called before the invocation of each test method in the class.
-	}
+	override func setUp() {}
 
-	override func tearDown() {
-		// Put teardown code here. This method is called after the invocation of each test method in the class.
-	}
+	override func tearDown() {}
 
-	func testAmountString() {
-		// Given:
-		// When:
-		// Then:
-	}
+	func testAmountString() {}
 
 	func testAmountForLtcFormat() {
 		// Given:
