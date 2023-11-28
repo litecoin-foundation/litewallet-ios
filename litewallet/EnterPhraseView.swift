e874156d4f77ab79458b3bea27c8e62c37478acd
kcw-grunt <mrkerrywashington@icloud.com>
2023-11-28 14:44:28 -0500

    Added new EnterPhraseView HostingController
    Signed-off-by: kcw-grunt <mrkerrywashington@icloud.com>

---
+++ litewallet/EnterPhraseView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct EnterPhraseView: View {
+	@ObservedObject
+	var viewModel: EnterPhraseViewModel
+
+	var body: some View {
+		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+	}
+}
+
+#Preview {
+	EnterPhraseView(viewModel: EnterPhraseViewModel())
+}
