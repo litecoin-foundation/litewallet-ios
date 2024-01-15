c0de3e9e6aa2b45f1d8e7971a2490fcb1753e2c8
kcw-grunt <mrkerrywashington@icloud.com>
2024-01-15 09:44:33 -0500

    cleanup of unused code
    Signed-off-by: kcw-grunt <mrkerrywashington@icloud.com>

--- litewallet/SendAddressCellView.swift
+++ litewallet/SendAddressCellView.swift
@@ -6,19 +6,10 @@ struct SendAddressCellView: View {
 	@ObservedObject
 	var viewModel = SendAddressCellViewModel()
 
-	@State
-	private var didStartEditing: Bool = false
-
-	@State
-	private var amountToggleTitle = "LTC (Ł)"
-
 	let actionButtonWidth: CGFloat = 45.0
 	let toggleButtonWidth: CGFloat = 60.0
 
 	let textFieldHeight: CGFloat = 45.0
-
-	private let currencyToggleConstant: CGFloat = 20.0
-	private let amountFont: UIFont = UIFont.barlowMedium(size: 14.0)
 
 	var body: some View {
 		GeometryReader { _ in
@@ -30,9 +21,6 @@ struct SendAddressCellView: View {
 					HStack {
 						VStack {
 							AddressFieldView(placeholder: S.Send.enterLTCAddressLabel.localize(), text: $viewModel.addressString)
-								.onTapGesture {
-									didStartEditing = true
-								}
 								.frame(height: textFieldHeight, alignment: .leading)
 						}
 						.padding(.leading, swiftUICellPadding)
@@ -95,50 +83,6 @@ struct SendAddressCellView: View {
 					)
 					.padding([.leading, .trailing], swiftUICellPadding)
 
-					/// Amount Field
-					HStack {
-						VStack {
-							AmountFieldView(placeholder: S.Send.amountLabel.localize(), text: $viewModel.amountString)
-								.onTapGesture {
-									didStartEditing = true
-								}
-								.frame(height: textFieldHeight, alignment: .leading)
-						}
-						.padding(.leading, swiftUICellPadding)
-
-						Spacer()
-
-						// Fiat LTC Switch Button
-						Button(action: {
-							viewModel.shouldScanAddress?()
-						}) {
-							HStack {
-								ZStack {
-									RoundedRectangle(cornerRadius: 4)
-										.frame(width: toggleButtonWidth, height: 30, alignment: .center)
-										.foregroundColor(Color(UIColor.secondaryButton))
-										.shadow(color: Color(UIColor.grayTextTint),
-										        radius: 3, x: 0, y: 4)
-										.padding(.all, 8.0)
-
-									Text(amountToggleTitle)
-										.frame(width: toggleButtonWidth, height: 30, alignment: .center)
-										.font(Font(UIFont.customMedium(size: 15.0)))
-										.foregroundColor(Color(UIColor.grayTextTint))
-										.overlay(
-											RoundedRectangle(cornerRadius: 4)
-												.stroke(Color(UIColor.secondaryBorder))
-												.frame(width: toggleButtonWidth, height: 30, alignment: .center)
-										)
-										.padding(.all, 8.0)
-								}
-							}
-						}
-					}
-					.background(
-						Color.white.clipShape(RoundedRectangle(cornerRadius: 8.0))
-					)
-					.padding([.leading, .trailing], swiftUICellPadding)
 					Spacer()
 				}
 			}
--- litewallet/SendAddressCellViewModel.swift
+++ litewallet/SendAddressCellViewModel.swift
@@ -25,30 +25,4 @@ class SendAddressCellViewModel: ObservableObject {
 
 	init()
 	{}
-
-	//    amountView.balanceTextForAmount = { [weak self] amount, rate in
-	//        self?.balanceTextForAmount(amount: amount, rate: rate)
-	//    }
-//
-	//    amountView.didUpdateAmount = { [weak self] amount in
-	//        self?.amount = amount
-	//    }
-	//    amountView.didUpdateFee = strongify(self) { myself, feeType in
-	//        myself.feeType = feeType
-	//        let fees = myself.store.state.fees
-//
-	//        switch feeType {
-	//            case .regular: myself.walletManager.wallet?.feePerKb = fees.regular
-	//            case .economy: myself.walletManager.wallet?.feePerKb = fees.economy
-	//            case .luxury: myself.walletManager.wallet?.feePerKb = fees.luxury
-	//        }
-//
-	//        myself.amountView.updateBalanceLabel()
-	//    }
-//
-	//    amountView.didChangeFirstResponder = { [weak self] isFirstResponder in
-	//        if isFirstResponder {
-	//            self?.memoCell.textView.resignFirstResponder()
-	//        }
-	//    }
 }
